84b339b673faf1ed98a303d4a9898d14e08ec45e
Sagar M <mankarisagar@gmail.com>
2026-03-08 09:12:58 -0400

    v1.4.1-beta

--- Casks/sparrow-terminal.rb
+++ Casks/sparrow-terminal.rb
@@ -1,6 +1,6 @@
 cask "sparrow-terminal" do
-  version "1.4.0-beta"
-  sha256 "310e42139cf9ac2b8ed12fa07aaad0bee7293ff41ebdd1774e0a90502a22c901"
+  version "1.4.1-beta"
+  sha256 "442e9b3dbba0dd223af0c722781f21aa923f51b5d975416be5ed992c49f509fd"
 
   url "https://github.com/sagarmk/sparrow-terminal/releases/download/v#{version}/sparrow-#{version}-arm64.dmg"
   name "Sparrow"
